744d47d381fc459f9d1025574c742f916445bfda
Casey O'Neil <caseyoneil@haleys-MacBook-Pro.local>
2022-06-30 12:15:50 -0700

    push to heroku

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Navbar from '../components/Navbar'
+import Navbar from '../components/NavBar'
 import About from '../components/About'
 import Main from '../components/Main'
 import Skills from '../components/Skills'
